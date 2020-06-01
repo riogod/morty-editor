074bf9716b080e9e61e36d6b0967d68eefcfcd1a
Vyacheslav Krasnyanskiy <riogod@gmail.com>
2020-06-01 13:14:47 +0300

    init project

---
+++ examples/slider.jsx
@@ -0,0 +1,12 @@
+/* eslint react/no-multi-comp: 0, max-len: 0 */
+import '../assets/index.less';
+
+import React from 'react';
+
+export default () => (
+  <div>
+    <div>
+      <p>Basic Slider</p>
+    </div>
+  </div>
+);
